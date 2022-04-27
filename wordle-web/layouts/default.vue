0d4f6b99f2b46995498a6f0515ac0631d0140397
Daniel Peterson <dpeterson8@ewu.edu>
2022-04-26 21:01:51 -0700

    Added About page

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -30,12 +30,19 @@
               </v-list-item-icon>
               <v-list-item-title>Home</v-list-item-title>
             </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item>
   
             <v-list-item>
               <v-list-item-icon>
                 <v-icon>mdi-account</v-icon>
               </v-list-item-icon>
-              <v-list-item-title>Account</v-list-item-title>
+              <v-list-item-title>Game</v-list-item-title>
             </v-list-item>
           </v-list-item-group>
         </v-list>
